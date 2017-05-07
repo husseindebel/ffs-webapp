1e8cf35375bdd8b157ad3730ae568849ad5bc5de
Hussein Debel <debelhussein@gmail.com>
2017-05-07 15:50:43 +1000

    fixed formatting

--- src/app/components/newstory/newstory.component.ts
+++ src/app/components/newstory/newstory.component.ts
@@ -1,21 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
 import 'rxjs/add/operator/switchMap';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import {NewsService} from '../../services/newsdata.service';
 import {OnInit} from '@angular/core';
 
-
 @Component({
     moduleId: module.id,
     selector: 'newstory',
     templateUrl: 'newstory.component.html',
-    // styleUrls: ['./newstory.component.css']
+    styleUrls: ['./newstory.component.css'],
     providers: [NewsService]
 })
 
 export class NewsStoryComponent implements OnInit{
 
-    story: Object;
+    story: NewsStory;
 
     constructor(private route: ActivatedRoute,
                 private router: Router,
@@ -28,9 +27,23 @@ export class NewsStoryComponent implements OnInit{
         this.route.queryParams
             .switchMap((params: Params) => this.service.getNewsbyHeadline(
                 params['headline']))
-            .subscribe((story) =>{
-                this.story = story;
-            })
+            .subscribe(
+                (story) => this.formatResults(story)
+            );
     }
 
+    formatResults(story){
+        console.log(story.NewsText);
+        this.story = story
+    }
+
+}
+
+interface NewsStory {
+    TopicCode: string[];
+    InstrumentIDs: string[];
+    Headline: string;
+    Date: string;
+    NewsText: string;
+    logfile: Object;
 }
